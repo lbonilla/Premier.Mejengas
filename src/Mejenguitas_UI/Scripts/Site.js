c39b02484c3b86880c8857903769be3d77e56aa6
Luis Bonilla H. <luisbonillah@gmail.com>
2013-09-25 16:25:24 -0600

    Missing changes

--- src/Mejenguitas_UI/Scripts/Site.js
+++ src/Mejenguitas_UI/Scripts/Site.js
@@ -1,1 +1,13 @@
-﻿$(".datepicker").datepicker();
+﻿$(".datepicker").datepicker({
+    dateFormat: 'dd/mm/yyyy'
+});
+
+function drawField() {
+    var c = document.getElementById("fieldCanvas");
+    if (c != null) {
+        var ctx = c.getContext("2d");
+        ctx.fillStyle = "#FF0000";
+        ctx.fillStyle(0, 0, 150, 75);
+    }
+}
+drawField();
